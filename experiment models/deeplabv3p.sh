1650bb6734b19a7a81628684db745fd4e8e0ddce
Yifei Gu <guyf0601@connect.hku.hk>
2023-04-19 09:34:54 +0800

    update sys config bash

--- experiment models/deeplabv3p.sh
+++ experiment models/deeplabv3p.sh
@@ -1,3 +1,12 @@
+'''
+Operating System: CentOS 8 (x86-64)
+HPC software stack: OpenHPC
+Job Scheduler: SLURM
+Parallel computing framework: PaddlePaddle
+
+Change the following variables to fit your own environment
+'''
+
 #!/bin/bash
 #SBATCH --job-name=deeplabv3p
 #SBATCH --partition=gpu
@@ -12,8 +21,8 @@
 #SBATCH --mail-user=YOUR_EMAIL
 
 source ~/.bashrc
-conda activate paddle_x3.9
-module load cuda
+conda activate paddle_x3.9  # specify your conda environment name
+module load cuda            # load cuda module on demand
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3 # use 4 GPUs
 
@@ -25,4 +34,6 @@ export FLAGS_sync_nccl_allreduce=1 # use nccl to do allreduce
 export FLAGS_enable_parallel_graph=1 # enable parallel graph mode
 '''
 
+# distribute training on 4 GPUs
+# if you only have 1 GPU, remove the '-m paddle.distributed.launch' command
 python -m paddle.distributed.launch --log_dir=output_deeplabv3p deeplabv3p.py
